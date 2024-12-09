7d7f22984dbc6592ce6d8842c49e07fcbc04c3b0
its-meet0123 <sukh.meet9896@gmail.com>
2024-12-09 13:55:57 +0530

    NAVBAR

---
+++ src/component/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src="logo.png"
+              alt="Avatar Logo"
+              style={{ width: 40 }}
+              className="rounded-pill"
+            />
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
