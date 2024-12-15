0dec1eb0aa9b50d97463da607c4db4788a1bfc5b
its-meet0123 <sukh.meet9896@gmail.com>
2024-12-15 18:34:13 +0530

    fontawesome laibrary not activeted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import Navbar from "./components/Navbar";
 import { workers } from "./components/Users";
 import { WorkerList } from "./components/Idcard";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 
 function App() {
   return (
     <>
       <Navbar />
-       
+      <FontAwesomeIcon icone = { faTwitter }/>
       <div className="container-fluid p-5 bg-secondary">
         <h1 className="text-center p-3">Worker's ID Cards</h1>
         <div className="row">
